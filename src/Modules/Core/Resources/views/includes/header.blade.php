d61237805c149dfad3de8cceca9267f56221df67
Matthias <matthias@refineddigital.co.nz>
2019-05-22 07:49:44 +0000

    Updating FontAwesome

--- src/Modules/Core/Resources/views/includes/header.blade.php
+++ src/Modules/Core/Resources/views/includes/header.blade.php
@@ -10,7 +10,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset('vendor/refinedcms/css/main.css?v='.uniqid()) }}"/>
     </head>
 
