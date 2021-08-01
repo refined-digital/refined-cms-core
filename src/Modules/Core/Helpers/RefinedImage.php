6975719909df49283d5237b616d73f1232584071
Matthias Law <matthias@refineddigital.co.nz>
2021-08-02 09:17:07 +1200

    feat: global pages override for image

--- src/Modules/Core/Helpers/RefinedImage.php
+++ src/Modules/Core/Helpers/RefinedImage.php
@@ -124,8 +124,13 @@ class RefinedImage {
             // only create if we are forcing, or the file doesn't already exist
             if (!file_exists($this->directory.$fileName) || $this->force) {
 
+                $quality = $this->quality;
+                if (config('pages.image.quality')) {
+                    $quality = (float) config('pages.image.quality');
+                }
+
                 // load the image
-                $image = Image::make($this->directory.$this->file->file, $this->quality);
+                $image = Image::make($this->directory.$this->file->file, $quality);
 
                 if ($this->type && $this->width && $this->height) {
                     if ($this->type == 'fit') {
