94f82a5e5c255ad0c1f763871119062fac27a3d3
Matthias Law <matthias@refineddigital.co.nz>
2022-04-14 20:10:13 +1200

    feat: adding session options to options field

--- src/Modules/Core/Resources/views/form/elements/select.blade.php
+++ src/Modules/Core/Resources/views/form/elements/select.blade.php
@@ -1,6 +1,25 @@
+@php
+  $options = (array) $field->options;
+  foreach ($options as $index => $opt) {
+      if (is_numeric(strpos($opt, 'session.'))) {
+          $key = str_replace('session.', '', $opt);
+          if (session()->has($key)) {
+              $data = session()->get($key);
+              unset($options[$index]);
+              if (is_array($data)) {
+                  foreach ($data as $id => $d) {
+                      $options[$id] = $d;
+                  }
+              } else {
+                  $options[$index] = $data;
+              }
+          }
+      }
+  }
+@endphp
 {!!
     html()
-        ->select($field->name, (array) $field->options)
+        ->select($field->name, $options)
         ->class('form__control')
         ->id('form--'.$field->name)
         ->attributes($attrs)
