30060b04dcade198ea7e82fbde1306708d4b75d6
Matthias Law <matthias@refineddigital.co.nz>
2022-10-10 08:19:43 +1300

    feat: adding noindex to cms admin

--- src/Modules/Core/Resources/views/includes/header.blade.php
+++ src/Modules/Core/Resources/views/includes/header.blade.php
@@ -8,6 +8,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="robots" content="noindex">
 
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
