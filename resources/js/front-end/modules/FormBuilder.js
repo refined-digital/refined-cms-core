107cb3620ed6a3febb189614fc27319badab1047
Matthias Law <matthias@refineddigital.co.nz>
2025-02-10 16:40:52 +1300

    feat: updating form builder scripts

--- resources/js/front-end/modules/FormBuilder.js
+++ resources/js/front-end/modules/FormBuilder.js
@@ -36,3 +36,132 @@ if (ccField.length) {
     });
   })
 }
+
+const forms = Array.from(document.querySelectorAll('form')).filter(form =>
+    form.className.match(/\bform--\d+\b/)
+);
+if (forms.length) {
+  const sendRequest = async (url, formElement , method = 'POST') => {
+    const headers = {
+      'Accept': 'application/json',
+      'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+    };
+
+    const body = new FormData(formElement);
+
+    const options = {
+      method,
+      headers,
+      credentials: 'same-origin'
+    };
+
+    if (body) {
+      options.body = body;
+    }
+
+    try {
+      const response = await fetch(url, options);
+      return await response.json();
+
+    } catch (error) {
+      console.error('Fetch error:', error);
+      throw error;
+    }
+  }
+
+  const formIsLoading = (button = null) => {
+    if (button) {
+      button.disabled = true;
+      button.classList.add('button--loading');
+    }
+  }
+  const formIsNotLoading = (button = null) => {
+    if (button) {
+      button.disabled = false;
+      button.classList.remove('button--loading');
+    }
+  }
+
+  const submit = async (form, button) => {
+    const formUrl = form.getAttribute('action');
+
+    try {
+      const response = await sendRequest(
+          formUrl,
+          form,
+      );
+
+      formIsNotLoading(button);
+
+      if (response.url) {
+        window.location.href = response.url;
+      }
+
+      if (response.confirmation) {
+        let element = form;
+
+        if (form.dataset.replacement) {
+          element = document.querySelector(`.${form.dataset.replacement}`);
+        }
+
+        if (element) {
+          if (element.tagName === 'FORM') {
+            element.insertAdjacentHTML('beforebegin', response.confirmation);
+            element.remove();
+          } else {
+            element.innerHTML = response.confirmation;
+          }
+        }
+
+      }
+
+
+    } catch (e) {
+      console.warn(e);
+      alert('There was an error submitting the form, please try again later');
+      formIsNotLoading(button);
+    }
+  }
+
+  forms.forEach(form => {
+    const validate = new window.FormValidate();
+    const tokenField = form.querySelector('input[name="_captcha"]');
+
+    form.addEventListener('submit', async function (e) {
+      e.preventDefault();
+      const submitButton = form.querySelector('.form__row--buttons .button');
+      const errors = validate.validate(this);
+
+      formIsLoading(submitButton);
+
+      if (errors.length) {
+        validate.alert();
+
+        formIsNotLoading(submitButton);
+      } else {
+        if (!tokenField) {
+          await submit(form, submitButton);
+        } else {
+          grecaptcha.ready(() => {
+            try {
+              grecaptcha
+                  .execute(form.dataset.red, { action: 'submit' })
+                  .then(async (token) => {
+                    tokenField.value = token;
+
+                    await submit(form, submitButton);
+                  })
+              ;
+            } catch (e) {
+              console.warn(e.message);
+              alert('An error has occurred, please try later.')
+              if (submitButton) {
+                formIsNotLoading(submitButton);
+              }
+            }
+          });
+        }
+      }
+    });
+  });
+}
