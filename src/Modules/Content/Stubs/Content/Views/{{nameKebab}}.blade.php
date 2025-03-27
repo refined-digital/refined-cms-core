c2e56b6ab6242be2470bc788b59507f9c5db8742
Matthias Law <matthias@refineddigital.co.nz>
2025-03-28 08:40:07 +1300

    fix: content block stub

--- src/Modules/Content/Stubs/Content/Views/{{nameKebab}}.blade.php
+++ src/Modules/Content/Stubs/Content/Views/{{nameKebab}}.blade.php
@@ -6,9 +6,9 @@
 <section class="{{ implode(' ', $classes) }}">
     <div class="holder">
         <article class="holder__body">
-            @include('templates.content.includes.heading')
-            @include('templates.content.includes.content')
-            @include('templates.content.includes.link')
+            @include('content-templates::includes.heading')
+            @include('content-templates::includes.content')
+            @include('content-templates::includes.link')
         </article>
     </div>
 </section>
