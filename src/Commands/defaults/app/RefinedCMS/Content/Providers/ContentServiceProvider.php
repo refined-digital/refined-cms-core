ad188917f3bbd8b881a7c3789b61d4cb7ab7c42a
Matthias Law <matthias@refineddigital.co.nz>
2025-03-27 15:50:26 +1300

    feat: remove some default content blocks

--- src/Commands/defaults/app/RefinedCMS/Content/Providers/ContentServiceProvider.php
+++ src/Commands/defaults/app/RefinedCMS/Content/Providers/ContentServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\RefinedCMS\Content\Providers;
 use App\RefinedCMS\Content\Blocks\Accordion\Accordion;
 use App\RefinedCMS\Content\Blocks\Banner\Banner;
 use App\RefinedCMS\Content\Blocks\Banners\Banners;
-use App\RefinedCMS\Content\Blocks\Content\BlogListing;
 use App\RefinedCMS\Content\Blocks\Form\Form;
 use App\RefinedCMS\Content\Blocks\FullWidthImage\FullWidthImage;
 use App\RefinedCMS\Content\Blocks\PlainContent\PlainContent;
@@ -72,10 +71,9 @@ class ContentServiceProvider extends ServiceProvider
             );
 
         // register the content fields
-        $agg->register(Accordion::class);
+        // $agg->register(Accordion::class);
         $agg->register(Banner::class);
         $agg->register(Banners::class);
-        $agg->register(BlogListing::class);
         $agg->register(Form::class);
         $agg->register(FullWidthImage::class);
         $agg->register(PlainContent::class);
