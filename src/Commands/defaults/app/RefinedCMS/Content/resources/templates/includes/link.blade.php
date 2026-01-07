2cdd71d06d3901defba35601199c33fa5f90f0e3
Matthias Law <matthias@refineddigital.co.nz>
2026-01-08 12:55:36 +1300

    fix: default class link

--- src/Commands/defaults/app/RefinedCMS/Content/resources/templates/includes/link.blade.php
+++ src/Commands/defaults/app/RefinedCMS/Content/resources/templates/includes/link.blade.php
@@ -1,11 +1,18 @@
 @if (isset($content->link) && $content->link)
     @php
-        $link = format()->linkAttributes($content->link);
+        $link = format()->linkAttributes((object) $content->link);
+        $classes = [
+            'button',
+        ];
+
+        if (isset($link->classes) && is_array($link->classes) && sizeof($link->classes)) {
+            $classes = array_merge($classes, $link->classes);
+        }
     @endphp
 
     @if ($link->link)
         <div class="content-link">
-            <a href="{{ $link->link }}"{!! $link->attributes ? ' '.$link->attributes : '' !!}{!! $link->classes ? ' class="'.implode($link->classes).'"' : '' !!}>{{ $link->title }}</a>
+            <a href="{{ $link->link }}"{!! $link->attributes ? ' '.$link->attributes : '' !!}{!! $classes ? ' class="'.implode($classes).'"' : '' !!}>{{ $link->title }}</a>
         </div>
     @endif
 @endif
