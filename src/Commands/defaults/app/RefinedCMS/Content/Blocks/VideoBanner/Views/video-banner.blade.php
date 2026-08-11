b7cac37bf07657839678d5e59b2a264a0ade5509
Matthias Law <matthias@refineddigital.co.nz>
2026-08-11 15:54:03 +1200

    feat: updating base styles

--- src/Commands/defaults/app/RefinedCMS/Content/Blocks/VideoBanner/Views/video-banner.blade.php
+++ src/Commands/defaults/app/RefinedCMS/Content/Blocks/VideoBanner/Views/video-banner.blade.php
@@ -3,14 +3,25 @@
       'page__block--no-padding',
     ]);
 
-    if (isset($content->vimeo_share_link)) {
-      $video = help()->getVimeoEmbedLink($content->vimeo_share_link, ['muted' => true, 'controls' => 0, 'loop' => 1, 'autoplay' => 1, 'background' => 1]);
+    if (isset($content->video->id) && $content->video->id) {
+        $video = files()->load($content->video->id)->url();
     }
+
 @endphp
 @if (isset($video) && $video)
-    <section class="{{ implode(' ', $classes) }}">
-        <div class="banner__video fade-in">
-            <iframe src="{{ $video }}" frameborder="0" allow="autoplay" class="video--desktop"></iframe>
-        </div>
+    <section class="banner banner--video {{ implode(' ', $classes) }}">
+        <video
+                class="banner__video"
+                autoplay
+                muted
+                loop
+                playsinline
+                webkit-playsinline
+                x-webkit-airplay="deny"
+                preload="auto"
+                disablePictureInPicture
+        >
+            <source src="{{ $video }}" type="video/mp4">
+        </video>
     </section>
 @endif
